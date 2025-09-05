60b1b2d6e72838b9e005fb87b374b04771bf13d7
Teq <1371117@gmail.com>
2025-09-06 02:26:12 +0800

    1.02a
    hotfix

--- mod/ParamHelpers.cpp
+++ mod/ParamHelpers.cpp
@@ -28,7 +28,7 @@ EquipParamGoods* ParamHelpers::GetEquipParamGoods(int itemId)
 
 unsigned char ParamHelpers::GetWeaponCategory(int itemId)
 {
-    EquipParamWeapon* params = GetEquipParamWeapon(itemId & 0xFFFFFFF0);
+    EquipParamWeapon* params = GetEquipParamWeapon(itemId & 0x0FFFFFFF);
 
     if (params)
     {
